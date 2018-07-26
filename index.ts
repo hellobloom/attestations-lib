12dabd5a08a5504cd3c28e69464edafc9d4ae443
Eddie Hedges <eddiehedges@gmail.com>
2018-07-26 11:24:22 -0500

    Attempting to fix package

---
+++ index.ts
@@ -0,0 +1,5 @@
+import {AttestationTypeID} from './src/AttestationTypeID'
+import {AttestationStatus} from './src/AttestationStatus'
+import * as AttestationUtils from './src/AttestationUtils'
+
+export {AttestationTypeID, AttestationStatus, AttestationUtils}
