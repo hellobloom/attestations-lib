f3f53a6297be99570e79ed87868b32db5525b13f
Eddie Hedges <eddiehedges@gmail.com>
2018-09-17 16:44:29 -0500

    Fixed incorrect typings

--- typings/merkletreejs.d.ts
+++ typings/merkletreejs.d.ts
@@ -1,16 +1,23 @@
 declare module 'merkletreejs' {
+  interface IOptions {
+    isBitcoinTree: boolean
+  }
+
+  interface IProof {
+    position: 'left' | 'right'
+    data: Buffer
+  }
+
   export default class MerkleTree {
     getRoot: () => Buffer
     getLeaves: () => Buffer[]
     getLayers: () => Buffer[]
-    getProof: (leaf: Buffer, index?: number) => Buffer[]
-    verify: (proof: Buffer[], targetNode: Buffer, root: Buffer) => boolean
+    getProof: (leaf: Buffer, index?: number) => IProof[]
+    verify: (proof: IProof[], targetNode: Buffer, root: Buffer) => boolean
     constructor(
       leaves: Buffer[],
       hashAlgorithm: (data: any) => Buffer,
-      options?: {
-        isBitcoinTree: boolean
-      }
+      options?: IOptions
     )
   }
 }
