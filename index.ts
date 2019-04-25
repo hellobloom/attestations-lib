b4eb04375ad1d2cc392c1289ed0045659b115eda
Isaac Patka <ipatka@gmail.com>
2019-04-25 11:57:01 -0400

    fix validation helper export

--- index.ts
+++ index.ts
@@ -15,6 +15,7 @@ import {
 } from './src/AttestationTypes'
 import * as AttestationData from './src/AttestationData'
 import * as HashingLogic from './src/HashingLogic'
+import * as Validation from './src/Validation'
 import * as RFC3339DateTime from './src/RFC3339DateTime'
 
 export {
@@ -33,5 +34,6 @@ export {
   getFormattedName,
   AttestationStatus,
   HashingLogic,
-  RFC3339DateTime
+  RFC3339DateTime,
+  Validation,
 }
